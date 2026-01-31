6d49a41689a184c35a43b0b96dc276f3fe1e65f7
Emanuel Castro <emanuel62893@gmail.com>
2026-01-31 16:31:41 -0500

    Allow duplicate account names

---
+++ In-Ex-Ledger-API/db/migrations/005_drop_accounts_name_unique_index.sql
@@ -0,0 +1,6 @@
+﻿-- =========================================
+-- Allow duplicate account names per business
+-- =========================================
+
+DROP INDEX IF EXISTS accounts_business_name_unique;
+
