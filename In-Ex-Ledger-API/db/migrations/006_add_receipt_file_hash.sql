f0760c38e17b1783b28be03c10a46482a2b986a3
Emanuel Castro <emanuel62893@gmail.com>
2026-02-03 17:15:35 -0500

    Add file_hash column to receipts

---
+++ In-Ex-Ledger-API/db/migrations/006_add_receipt_file_hash.sql
@@ -0,0 +1,6 @@
+﻿-- =========================================
+-- Add file_hash column to receipts
+-- =========================================
+
+ALTER TABLE receipts
+ADD COLUMN IF NOT EXISTS file_hash TEXT;
