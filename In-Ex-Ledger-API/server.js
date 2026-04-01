c7b9eb92e54ced5423acf165768c8f651cdcd296
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-04-01 09:16:17 -0400

    Update server.js
    Added app.use(express.json({ limit: "100kb" })); app.use(express.urlencoded({ extended: false, limit: "100kb" }));

--- In-Ex-Ledger-API/server.js
+++ In-Ex-Ledger-API/server.js
@@ -46,7 +46,8 @@ app.use(cors({
    ========================================================= */
 app.use(express.static(publicDir));
 app.use(express.static(path.join(publicDir, "html")));
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: false, limit: "100kb" }));
 app.use(cookieParser());
 
 /* =========================================================
