290df8e9a934607ba5e3b99aee2b4a01ad1f60e7
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-05-13 07:21:34 -0400

    Update messages.js

--- In-Ex-Ledger-API/public/js/messages.js
+++ In-Ex-Ledger-API/public/js/messages.js
@@ -44,6 +44,7 @@ let _currentFilter = "messages";
 let _currentMsgId = null;
 let _currentMsgReceiverId = null;
 let _currentReplyMode = "in-app";
+let _currentThread = [];
 let _contacts = [];
 let _mailboxMessages = [];
 let _lastRefreshAt = null;
